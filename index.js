9155e5cabfdb7a342db03c71660243c43004bc6f
Mads Hvelplund <mads.hvelplund@ufst.dk>
2020-11-09 13:26:08 +0000

    Added commandline parameters

--- index.js
+++ index.js
@@ -4,34 +4,28 @@
 
 const fs = require('fs');
 const path = require('path');
-const cp = require('child_process');
-
-const trace = require('debug')("TRACE");
 const debug = require('debug')("DEBUG");
-
-const tfModulePath = ".terraform/modules"
-
-// Determine if a Terraform module source string is a Git reference
-const isGitSource = o => o.startsWith("git") || o.startsWith("bitbucket.org")
+const program = require("commander");
+const package_json = require("./package.json");
 
 // Get a list of files with the ".tf" file-ending
 function getTfFiles(dir) {
-    const regex = /\.tf$/
+    const regex = /\.tf$/;
     return fs.readdirSync(dir)
         .map(f => path.join(dir, f))
         .filter(f => regex.test(f))
         .filter(f => {
             try {
-                return !fs.statSync(f).isDirectory()
+                return !fs.statSync(f).isDirectory();
             } catch (err) {
-                debug(`Skipping "${f}: ${err.message}"`)
-                return false // Skip stuff like broken symlinks or unreadable files
+                debug(`Skipping "${f}: ${err.message}"`);
+                return false; // Skip stuff like broken symlinks or unreadable files
             }
-        })
+        });
 }
 
 function scan(root, prefix) {
-    prefix = prefix || ""
+    prefix = prefix || "";
 
     const matches = getTfFiles(root)
         .map(file => fs.readFileSync(file, { encoding: 'utf8', flag: 'r' }))
@@ -47,27 +41,47 @@ function scan(root, prefix) {
         .flat()
         .reduce((map, obj) => {
             map[`${prefix}${obj.module}`] = {
-                id: id++,
+                id: id += 1,
                 key: obj.module,
                 dependencies: new Set(Array.from(obj.body.matchAll(/module\.([-\w]+)\.([-\w]+)/g), m => prefix + m[1])),
                 //edges: Array.from(obj.body.matchAll(/module\.([-\w]+)\.([-\w]+)/g), m => { return { target: m[1], label: m[2] } })
             }
             return map
-        }, {})
+        }, {});
 
     for (const [path, value] of Object.entries(matches)) {
+        let skip = false;
+        for (i = 0; i < filterNames.length; i += 1) {
+            if (value.key.match(filterNames[i])) {
+                skip = true;
+                break;
+            }
+        }
+
+        if (skip) {
+            continue;
+        }
+
         vertices[path] = value;
 
         parent = path.substring(0, path.lastIndexOf('.'))
-        if (parent.length > 0) vertices[parent].dependencies.add(path)
+        if (parent.length > 0) {
+            if (addDepFromParent) {
+                vertices[parent].dependencies.add(path);
+            }
+            if (parentIsGroup) {
+                vertices[parent].isGroup = true;
+                vertices[path].groupId = vertices[parent].id;
+            }
+        }
 
-        debug(`Scanning "${path} in "${root}"`)
-        let cache
+        debug(`Scanning "${path} in "${root}"`);
+        let cache;
 
         try {
-            cache = modules[path].dir
+            cache = modules[path].dir;
         } catch (err) {
-            debug(`ERROR: Can't look up "${path}": ${err.message}. Skipping.`)
+            debug(`ERROR: Can't look up "${path}": ${err.message}. Skipping.`);
             continue;
         }
 
@@ -104,9 +118,14 @@ function printGml(vertices) {
         parent = key.substring(0, key.lastIndexOf('.'))
         console.log(`  node [
     id ${value.id}
-    label "${value.key}
-${parent}"
-  ]`)
+    label "${value.key}"`);
+        if (value.groupId) {
+            console.log(`    gid ${value.groupId}`)
+        }
+        if (value.isGroup) {
+            console.log("    isGroup 1")
+        }
+        console.log("  ]")
         value.dependencies.forEach((dependency, key, set) => {
             try {
                 edges.push(`  edge [
@@ -124,20 +143,68 @@ ${parent}"
 }
 
 
+
+program.version(package_json.version).description(package_json.description);
+
+program
+    .option(
+        "-p, --tf-module-path <path>",
+        `the path where "terraform init" has downloaded the module cache`,
+        ".terraform/modules"
+    )
+    .option(
+        "-d, --parent-depends-on-child",
+        `add dependencies from parents to their children`,
+        true
+    )
+    .option(
+        "-g, --parent-is-group",
+        `make parents a group containing their children`,
+        false
+    )
+    .option(
+        "-f, --output-format <format>",
+        `The output format (gml|tgf)`,
+        "gml"
+    );
+
+program.parse(process.argv);
+
+const tfModulePath = program.tfModulePath
+const addDepFromParent = program.parentDependsOnChild;
+const parentIsGroup = program.parentIsGroup;
+// const useTgf = false;
+// const useGml = true;
+const filterNames = [
+    /^naming$/,
+    /^tagging$/
+];
+
 // Load the current modules.json file into memory
-const modulesJson = JSON.parse(fs.readFileSync(`${tfModulePath}/modules.json`))
-
-const modules = modulesJson.Modules
-    .reduce((map, obj) => {
-        map[obj.Key] = { dir: obj.Dir, source: obj.Source }
-        return map
-    }, {})
-
-let id = 0
-const vertices = {}
 
 // Start scanning for modules, modifying the edge list and vertex list along the way
-scan(".")
+try {
+    const modulesJson = JSON.parse(fs.readFileSync(`${tfModulePath}/modules.json`))
 
-// printTgf(vertices)
-printGml(vertices)
+    const modules = modulesJson.Modules
+        .reduce((map, obj) => {
+            map[obj.Key] = { dir: obj.Dir, source: obj.Source }
+            return map
+        }, {})
+
+    let id = 0
+    const vertices = {}
+
+    scan(".")
+
+    if (program.outputFormat == "tgf") {
+        printTgf(vertices);
+    } if (program.outputFormat == "gml") {
+        printGml(vertices);
+    } else {
+        console.log(vertices);
+    }
+} catch (err) {
+    console.error("Failed generate graph:", err.message, "\n");
+    program.outputHelp();
+}
