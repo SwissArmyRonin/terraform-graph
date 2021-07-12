c4b50b650e1776d6f9b21c8c17eddeac1e4c3f05
Mads Hvelplund <mads.hvelplund@ufst.dk>
2021-07-12 10:32:35 +0200

    Added example module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("OK")
+const execSync = require('child_process').execSync;
+
+function getTfJson(targetDir: string, hclToJsonPath?: string) {
+    hclToJsonPath = hclToJsonPath || "hcl2json"
+    const output: string = execSync(`cat ${targetDir}/*.tf | ${hclToJsonPath}`, { encoding: 'utf-8' });
+    return JSON.parse(output);
+}
+
+var targetDir: string = "example/terraform-aws-eks"
+console.log('Output was:\n', getTfJson(targetDir));
